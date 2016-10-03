805c8009229b0f5dd6688afd89254857d86903a1
lfaler <laynefaler@gmail.com>
2016-10-02 17:23:22 -0700

    Initial commit

---
+++ project/client/js/App.jsx
@@ -0,0 +1,21 @@
+/**
+* @Author: Layne Faler <laynefaler>
+* @Date:   10-02-2016
+* @Email:  laynefaler@gmail.com
+* @Last modified by:   laynefaler
+* @Last modified time: 10-02-2016
+*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, Link } from 'react-router';
+
+class App extends React.Component {
+  render() {
+    return (
+      <h1>We Love You Hilary!</h1>
+    );
+  }
+}
+
+export default App;
---
+++ project/client/js/Main.jsx
@@ -0,0 +1,21 @@
+/**
+* @Author: Layne Faler <laynefaler>
+* @Date:   10-02-2016
+* @Email:  laynefaler@gmail.com
+* @Last modified by:   laynefaler
+* @Last modified time: 10-02-2016
+*/
+
+import React from 'react';
+
+import App from './App';
+
+var Main = React.createClass({
+  render: function() {
+    return (
+      <App />
+    );
+  }
+});
+
+export default Main;
