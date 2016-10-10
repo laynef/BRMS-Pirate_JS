1b796b16614dcfed97fbdd3759371429114902c7
lfaler <laynefaler@gmail.com>
2016-10-10 15:19:23 -0700

    Added medteor projecy 'temp' and text and print document commands

---
+++ actions/index.js
@@ -0,0 +1,9 @@
+/**
+* @Author: Layne Faler <laynefaler>
+* @Date:   10-10-2016
+* @Email:  laynefaler@gmail.com
+* @Last modified by:   laynefaler
+* @Last modified time: 10-10-2016
+*/
+
+// delete header
